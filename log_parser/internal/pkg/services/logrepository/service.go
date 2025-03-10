d38615f28c22d4496f0ead5960e7915497559430
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-10 23:44:18 +0100

    TEST pt.1

--- log_parser/internal/pkg/services/logrepository/service.go
+++ log_parser/internal/pkg/services/logrepository/service.go
@@ -1,11 +1,5 @@
 package logrepository
 
-import (
-	"fmt"
-	"os"
-	"path/filepath"
-)
-
 const (
 	logsStorageDirectory = "/logs/"
 	logFileNamePattern   = "l*.log"
@@ -18,26 +12,27 @@ func NewService() *Service {
 }
 
 func (s *Service) GetLogs() (map[string][]byte, error) {
-	pattern := filepath.Join(logsStorageDirectory, logFileNamePattern)
-	files, err := filepath.Glob(pattern)
-	if err != nil {
-		return nil, fmt.Errorf("failed to search for log files: %w", err)
-	}
+	// pattern := filepath.Join(logsStorageDirectory, logFileNamePattern)
+	// files, err := filepath.Glob(pattern)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failed to search for log files: %w", err)
+	// }
 
-	if files == nil {
-		return nil, nil
-	}
+	// if files == nil {
+	// 	return nil, nil
+	// }
 
-	// map [ file name ] -> content
-	logs := make(map[string][]byte)
+	// // map [ file name ] -> content
+	// logs := make(map[string][]byte)
 
-	for _, file := range files {
-		data, err := os.ReadFile(file)
-		if err != nil {
-			return nil, fmt.Errorf("reading logs error: %s: %w", file, err)
-		}
-		logs[file] = data
-	}
+	// for _, file := range files {
+	// 	data, err := os.ReadFile(file)
+	// 	if err != nil {
+	// 		return nil, fmt.Errorf("reading logs error: %s: %w", file, err)
+	// 	}
+	// 	logs[file] = data
+	// }
 
-	return logs, nil
+	// return logs, nil
+	return nil, nil
 }
