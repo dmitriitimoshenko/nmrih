605a126830dcf6c3370c1ce1a8826dee90d26d54
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-14 22:06:10 +0100

    second attempt to get fine logs

--- log_frontend/src/App.js
+++ log_frontend/src/App.js
@@ -10,7 +10,7 @@ import './App.css';
 function App() {
   const { chartData, loading, refreshData } = useGraphData();
 
-  const { windowDimensions } = useWindowDimensions
+  const { windowDimensions } = useWindowDimensions();
   console.log(windowDimensions)
 
   return (
