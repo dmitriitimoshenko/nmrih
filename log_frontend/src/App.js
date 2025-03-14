325e892d91c57a6e45d2923936b063d619fbdd15
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-14 22:15:46 +0100

    testing: correct display order in narrow devices

--- log_frontend/src/App.js
+++ log_frontend/src/App.js
@@ -11,8 +11,47 @@ function App() {
   const { chartData, loading, refreshData } = useGraphData();
 
   const { height, width } = useWindowDimensions();
-  console.log(useWindowDimensions())
   console.log(height, width)
+  // if width < 800 => recognize as phone screen / place top countries and player info components one under another
+
+  if (width < 800) {
+    return (
+      <div className="App">
+  
+        <h1>Krich Casual NMRiH Server Dashboard</h1>
+        <Controls onRefresh={refreshData} loading={loading} />
+        
+        <table>
+          <tbody>
+            <tr>
+              <td>
+                <h3>Top Time-spent Players</h3>
+                <div className="graph-container">
+                  <TopTimeChart data={chartData} />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3>Top Countries</h3>
+                <div className="pie-chart-container">
+                  <CountryPieChart />
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>
+                <h3>Player Info</h3>
+                <div className="players-info">
+                  <PlayersInfo />
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 
   return (
     <div className="App">
