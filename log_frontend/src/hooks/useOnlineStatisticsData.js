6bb2fafa0e418ec37c8210bd9657fb35ac1d782d
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-15 13:16:45 +0100

    hook added to FE

---
+++ log_frontend/src/hooks/useOnlineStatisticsData.js
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+
+const useOnlineStatisticsData = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://log-parser.rulat-bot.duckdns.org/api/v1/graph?type=online-statistics")
+      .then(response => response.json())
+      .then(jsonData => {
+        if (jsonData && jsonData.data) {
+          setData(jsonData.data);
+        }
+      })
+      .catch(err => {
+        console.error("Error fetching online statistics data:", err);
+      })
+  }, []);
+
+  return { data };
+};
+
+export default useOnlineStatisticsData;
