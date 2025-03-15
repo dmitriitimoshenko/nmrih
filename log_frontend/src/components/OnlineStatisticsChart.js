321faba7fee5169314d43ddc9f87b5fe597e60a2
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-15 16:00:20 +0100

    new graph fix 1

--- log_frontend/src/components/OnlineStatisticsChart.js
+++ log_frontend/src/components/OnlineStatisticsChart.js
@@ -30,6 +30,8 @@ const OnlineStatisticsChart = () => {
     return <p style={{ color: '#fff' }}>No data available for the online statistics chart.</p>;
   }
 
+  console.log(data);
+
   return (
     <ResponsiveContainer width="100%" height={400}>
       <LineChart data={data} margin={{ top: 20, right: 30, left: 20, bottom: 5 }}>
