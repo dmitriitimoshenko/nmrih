6b84cfddaa16c006b6789c111ac5b0987a8027c2
Timo <dmitrii.timoshenko@everphone.de>
2025-03-18 14:07:52 +0100

    nolint regex expr

--- log_api/internal/tools/regex.go
+++ log_api/internal/tools/regex.go
@@ -2,6 +2,7 @@ package tools
 
 import "regexp"
 
+//nolint:lll // This regex cannot be split
 var IPRegex = regexp.MustCompile(
 	`^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$`,
 )
