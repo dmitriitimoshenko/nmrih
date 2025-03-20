d594249d623ed24f5da710a2eaf812fc0f57ed45
Dmitrii <95302336+dmitriitimoshenko@users.noreply.github.com>
2025-03-20 11:36:58 +0100

    minor improvements pt 4 (#21)

--- log_api/internal/tools/regex_test.go
+++ log_api/internal/tools/regex_test.go
@@ -14,10 +14,14 @@ func TestIPRegex(t *testing.T) {
 	assert.Equal(t, true, tools.IPRegex.MatchString("0.0.0.0"))
 	assert.Equal(t, true, tools.IPRegex.MatchString("255.255.255.255"))
 
-	// assert.Equal(t, false, tools.IPRegex.MatchString("256.255.255.255"))
+	assert.Equal(t, false, tools.IPRegex.MatchString("256.255.255.255"))
 	// assert.Equal(t, false, tools.IPRegex.MatchString("-1.255.255.255"))
 	assert.Equal(t, false, tools.IPRegex.MatchString("255.255.255"))
 	assert.Equal(t, false, tools.IPRegex.MatchString(".255.255.255"))
 	assert.Equal(t, false, tools.IPRegex.MatchString("255.255.255."))
 	assert.Equal(t, false, tools.IPRegex.MatchString("255.255.255.INVALID"))
+
+	assert.True(t, tools.IPRegex.MatchString(
+		`L 03/15/2025 - 16:05:12: "BigZeeb<69><[U:1:1419671927]><>" connected, address "173.172.252.206:27005"`,
+	))
 }
