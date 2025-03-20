b4c01e01045dd326b96c8d6c4016fda3565a33cb
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-20 11:35:45 +0100

    minor improvements pt 4

--- log_api/internal/tools/regex.go
+++ log_api/internal/tools/regex.go
@@ -2,4 +2,4 @@ package tools
 
 import "regexp"
 
-var IPRegex = regexp.MustCompile(`\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b`)
+var IPRegex = regexp.MustCompile(`\b(?:(25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(25[0-5]|2[0-4]\d|[01]?\d\d?)\b`)
