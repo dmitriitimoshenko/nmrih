75ac094538c5a5144fd89311acdf3d5a38994adc
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-20 21:29:38 +0100

    testing pt.2

--- log_api/internal/pkg/services/logparser/service.go
+++ log_api/internal/pkg/services/logparser/service.go
@@ -167,6 +167,7 @@ func (s *Service) mapLogs(logs map[string][]byte, dateFrom time.Time) ([]dto.Log
 			}
 
 			if err := data.Validate(); err != nil {
+				log.Println("[!!!] Failed to validate log data entry: ", err)
 				return nil, fmt.Errorf("[!!!] Failed to validate log data entry: %w", err)
 			}
 
