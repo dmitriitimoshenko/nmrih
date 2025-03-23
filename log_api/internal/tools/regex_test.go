b2c1f882a37cd3fdb2a081e046ef08db50596f1c
dmitrii <dmitrii.timoshenko16@gmail.com>
2025-03-23 14:09:00 +0100

    regex fix: updated: L is expcted in the beginning

--- log_api/internal/tools/regex_test.go
+++ log_api/internal/tools/regex_test.go
@@ -22,15 +22,20 @@ func TestIPRegex(t *testing.T) {
 	assert.Equal(t, false, tools.IPRegex.MatchString("255.255.255.INVALID"))
 
 	assert.True(t, tools.IPRegex.MatchString(
-		`L 03/15/2025 - 16:05:12: "BigZeeb<69><[U:1:1419671927]><>" connected, address "173.172.252.206:27005"`,
+		`L 03/15/2025 - 16:05:12: "BigZeeb<69><[U:1:xxxxxxxxxx]><>" connected, address "123.190.1.1:27005"`,
 	))
 }
 
 func TestDateTimeRegex(t *testing.T) {
-	assert.True(t, tools.DateTimeRegex.MatchString("01/01/2025 - 00:00:00"))
-	assert.True(t, tools.DateTimeRegex.MatchString("31/12/2099 - 23:59:59"))
-	assert.False(t, tools.DateTimeRegex.MatchString("1/01/2025 - 00:00:00"))
-	assert.False(t, tools.DateTimeRegex.MatchString("01/01/2025 - 0:00:00"))
-	assert.False(t, tools.DateTimeRegex.MatchString("a1/01/2025 - 00:00:00"))
-	assert.False(t, tools.DateTimeRegex.MatchString("01/01/2025  00:00:00"))
+	assert.False(t, tools.DateTimeRegex.MatchString("01/01/2025 - 00:00:00"))
+
+	assert.True(t, tools.DateTimeRegex.MatchString(
+		`L 03/23/2025 - 08:05:10: "XXXXX<101><[U:1:xxxxxxxxxx]><>" committed suicide with "world"`,
+	))
+	assert.False(t, tools.DateTimeRegex.MatchString(
+		`L 03/23/2X25 - 08:05:10: "XXXXX<101><[U:1:xxxxxxxxxx]><>" committed suicide with "world"`,
+	))
+	assert.False(t, tools.DateTimeRegex.MatchString(
+		`L 03/23/2025 08:05:10: "XXXXX<101><[U:1:xxxxxxxxxx]><>" committed suicide with "world"`,
+	))
 }
